33ef0f145e89084394aa28d73fdca358f9276447
subair7 <subairmusthafa1991@gmail.com>
2024-02-18 23:31:08 +0530

    Sample Files

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,16 +1,7 @@
-main() {
-  List list = [4, 8, 2, 6, 1, 7, 9, 1, 5, 3];
-  List result = [];
-  result.add(list[0]);
-  for (int i = 0; i < list.length; i++) {
-    for (int j = i + 1; j < list.length; j++) {
-      if (list[j] > list[i]) {
-        result.remove(i);
-        result.add(list[j]);
-      }
-    }
-  }
-  print(result);
+List<int> first(List<int> list) => list.sublist(0,list.length~/2).sort();
 
-  // print(list.fold(list[0], (p, e) => p>e?p));
+void main(List<String> args) {
+  List<int> l = [1, 8,3,4, 3, 2];
+  first(l);
+  print(l);
 }
