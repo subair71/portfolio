adee81cd6b7090a08523606eb484506c4129b786
subair7 <subairmusthafa1991@gmail.com>
2024-02-18 23:45:58 +0530

    List changed in portfolio

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,7 +1,7 @@
 List<int> first(List<int> list) => list.sublist(0,list.length~/2).sort();
 
 void main(List<String> args) {
-  List<int> l = [1, 8,3,4, 3,4,8, 2];
+  List<int> l = [1, 8,3,4, 3,4,8, 2,1,4];
   first(l);
   print(l);
 }
