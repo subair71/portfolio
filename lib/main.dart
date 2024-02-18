2415656135f0b376358419204c614280940abf30
subair7 <subairmusthafa1991@gmail.com>
2024-02-19 00:25:57 +0530

    Main File Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/res/constants.dart';
+import 'package:flutter_portfolio/view/splash/splash_view.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String title = 'old';
-  void updateTitle() {
-    setState(() {
-      title = 'new';
-    });
-
-    // setState() {
-    //   title = "new";
-    //   print("h");
-    // }
-  }
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: bgColor,
+        useMaterial3: true,
+        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: const Color.fromARGB(255, 11, 11, 11),)
+            .copyWith(
+          bodyText1: const TextStyle(color: bodyTextColor),
+          bodyText2: const TextStyle(color: bodyTextColor),
+        ),
       ),
-      home: MyHomePage(
-        title: title,
-        onPressed: updateTitle,
-      ),
+
+      home: SplashView()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-  final VoidCallback onPressed;
-  const MyHomePage({Key? key, required this.title, required this.onPressed})
-      : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void didUpdateWidget(covariant MyHomePage oldWidget) {
-    // TODO: implement didUpdateWidget
-    super.didUpdateWidget(oldWidget);
-    if (this.widget.title != oldWidget.title) {
-      print("Configuration has Changed");
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: widget.onPressed, child: Icon(Icons.abc)),
-          body: Center(
-            
-            child: PhysicalModel(
-              color: Colors.blue,
-              elevation: 8.0,
-              shadowColor: Colors.deepOrange,
-              child: Container(
-                // color: Colors.amber,
-                height: 100,width: 100,),
-            )),
-    );
-  }
-}
