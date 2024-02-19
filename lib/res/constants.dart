81f3ee2bb75791e04bd45f3daaa427c00ddf647b
subair7 <subairmusthafa1991@gmail.com>
2024-02-19 05:48:22 +0530

    Constants

---
+++ lib/res/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// const primaryColor = Colors.white;
+const primaryColor = Color.fromARGB(255, 11, 87, 240);
+
+const secondaryColor = Color(0xFF242430);
+const darkColor = Color(0xFF191923);
+const bodyTextColor = Color(0xFF8B8B8D);
+// const bgColor = Color(0xFF000515);
+const bgColor = Colors.white;
+
+const defaultPadding = 20.0;
