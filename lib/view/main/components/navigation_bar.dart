fcc7ba805d3128a03e3a5f88821a0e4fc095eee9
Subair K M <subair@bluesharksolution.ca>
2024-11-08 00:57:59 +0530

    tests updated5

--- lib/view/main/components/navigation_bar.dart
+++ lib/view/main/components/navigation_bar.dart
@@ -9,22 +9,22 @@ class TopNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(defaultPadding),
-            child:!Responsive.isLargeMobile(context)? Image.asset('assets/images/triange_icon.png') : MenuButton(onTap: () => Scaffold.of(context).openDrawer(),),
-          ),
-          // if(Responsive.isLargeMobile(context)) MenuButton(),
-          const Spacer(flex: 2,),
-          if(!Responsive.isLargeMobile(context))  const NavigationButtonList(),
-          const Spacer(flex: 2,),
-          const ConnectButton(),
-          const Spacer(),
-        ],
-      ),
+      // body: Row(
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: [
+      //     const Spacer(),
+      //     Padding(
+      //       padding: const EdgeInsets.all(defaultPadding),
+      //       child:!Responsive.isLargeMobile(context)? Image.asset('assets/images/triange_icon.png') : MenuButton(onTap: () => Scaffold.of(context).openDrawer(),),
+      //     ),
+      //     // if(Responsive.isLargeMobile(context)) MenuButton(),
+      //     const Spacer(flex: 2,),
+      //     if(!Responsive.isLargeMobile(context))  const NavigationButtonList(),
+      //     const Spacer(flex: 2,),
+      //     const ConnectButton(),
+      //     const Spacer(),
+      //   ],
+      // ),
     );
   }
 }
